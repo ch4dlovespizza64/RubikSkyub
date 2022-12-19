eb26d70507c1396097d7e30848a0ff8b0bb2aeeb
ch4dlovespizza64 <104768978+ch4dlovespizza64@users.noreply.github.com>
2022-12-19 18:31:16 +0800

    Basic Humanoid Manager
    A Roblox GUI that manages the basic properties of the HumanoidRootPart.

--- Basic_Humanoid_Manager.lua
+++ Basic_Humanoid_Manager.lua
@@ -1,1 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ch4dlovespizza64/MySourceCode/main/RubikSHUMANOID.lua"))()
+
+-- Key is: rubicscube
+
+-- Made by RTLua/Ch4d/random king
+
+-- Subscribe: https://www.youtube.com/@rtlua
