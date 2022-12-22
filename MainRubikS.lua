d4aabc1c1ff98efcacc9708bd0df98e86fc9f58c
ch4dlovespizza64 <104768978+ch4dlovespizza64@users.noreply.github.com>
2022-12-22 14:11:20 +0800

    Loadstring of RubikSkyub
    Unless your computer can't load a bunch of obfuscation, This code will immediately do its magic!

---
+++ MainRubikS.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ch4dlovespizza64/RubikSkyub/main/RubikSkyub.lua"))()
+--[[
+RubikSkyub v1.4
+Updates in the script
+Made by RTLua1/ch4dlovespizza/Random King
+
+Key: rubikscube
+(NOTE: key changes every month, to keep updated, please check on my discord server for the key!)
+Discord Server: https://discord.gg/FDHZSTncEP
+--]]
