145cd0d149ff02748ad9d1cc156350222146d047
ch4dlovespizza64 <104768978+ch4dlovespizza64@users.noreply.github.com>
2022-12-22 14:08:07 +0800

    RubikSkyub
    Inspired by the idea of the Rubik's Cube, this Roblox script modifier has a lot of combinations that can be solved into one! Use it properly or the game might break!
    Universal Script Loader and Modifier
    Key: rubikscube

--- Basic_Humanoid_Manager.lua
+++
@@ -1,7 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ch4dlovespizza64/MySourceCode/main/RubikSHUMANOID.lua"))()
-
--- Key is: rubikscube
-
--- Made by RTLua/Ch4d/random king
-
--- Subscribe: https://www.youtube.com/@rtlua
